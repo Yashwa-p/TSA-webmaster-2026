5e514eb81eb9f30f43d4bb19a1c4d60a526b74df
Yashwa-p <165945725+Yashwa-p@users.noreply.github.com>
2026-02-12 18:12:38 -0500

    Update script.js

--- script.js
+++ script.js
@@ -39,30 +39,3 @@ window.addEventListener('scroll', () => {
   if (window.scrollY > 80) header.classList.add('scrolled');
   else header.classList.remove('scrolled');
 });
-
-const track = document.querySelector('.carousel-track');
-const slides = document.querySelectorAll('.slide');
-const nextBtn = document.querySelector('.next');
-const prevBtn = document.querySelector('.prev');
-
-let currentIndex = 0;
-
-function updateCarousel() {
-  track.style.transform = `translateX(-${currentIndex * 100}%)`;
-}
-
-nextBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateCarousel();
-});
-
-prevBtn.addEventListener('click', () => {
-  currentIndex = (currentIndex - 1 + slides.length) % slides.length;
-  updateCarousel();
-});
-
-/* Auto slide */
-setInterval(() => {
-  currentIndex = (currentIndex + 1) % slides.length;
-  updateCarousel();
-}, 5000);
